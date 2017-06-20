ed1e0f3844643f81e11ee2aac581f3a57ff1da8c
Bartlomiej (@VM) <b.sielski@yandex.com>
2017-06-20 22:03:08 +0200

    Add act_as_list method

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,9 +1,13 @@
 class Category < ApplicationRecord
+
+  
   has_many :categorizations
   has_many :articles, through: :categorizations
 
+  acts_as_list
+  
   def self.default
     NonCategory.new
   end
-
+  
 end
