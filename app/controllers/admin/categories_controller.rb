f4f4734a7ead557c9edc91f905160f7a573d2fc5
Bartlomiej (@VM) <b.sielski@yandex.com>
2017-06-21 20:58:48 +0200

    Delete and destroy action for categories

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,5 +1,7 @@
 class Admin::CategoriesController < ApplicationController
 
+  before_action :set_category, only: [:edit, :update, :delete, :destroy]
+
   def new
     @category = Category.new
   end
@@ -7,7 +9,7 @@ class Admin::CategoriesController < ApplicationController
   def create
     @category = Category.new(category_params)
     if @category.save
-      redirect_to categories_path, notice: 'Category was successfully created.'
+      redirect_to admin_categories_path, notice: 'Category was successfully created.'
     else
       render :new
     end
@@ -19,8 +21,21 @@ class Admin::CategoriesController < ApplicationController
   def update
   end
 
-  private
+  def delete
+
+  end
   
+  def destroy
+    @category.destroy
+    redirect_to admin_categories_path, notice: 'Category was successfully destroyed.'
+  end
+  
+  private
+
+  def set_category
+    @category = Category.find(params[:id])
+  end
+    
   def category_params
     params.require(:category).permit(:name, :description)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   devise_for :admins
   
   namespace :admin do
-    resources :categories, only: [:index, :new, :edit] do
+    resources :categories, only: [:index, :new, :create, :edit, :delete, :destroy] do
+      member do
+        get 'delete'
+      end
       resources :articles
     end
   end
