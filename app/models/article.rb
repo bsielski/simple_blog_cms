fa0d3e3f000f355374d1ed4e14b5eaaff039cb66
Bartlomiej (@VM) <b.sielski@yandex.com>
2017-06-22 19:29:39 +0200

    Make convertion to html

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,14 @@ class Article < ApplicationRecord
   belongs_to :author
   has_many :categorizations
   has_many :categories, through: :categorizations
+  before_validation :convert_content_to_html, on: [:create, :update]
+
+
+
+  private
+
+  def convert_content_to_html
+    self.content = Convert.to_html self.content
+  end
+
 end
