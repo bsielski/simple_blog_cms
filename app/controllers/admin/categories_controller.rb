5151724e2a30d509884449ae725a68233fdf645d
Bartlomiej (@VM) <b.sielski@yandex.com>
2017-06-23 18:36:16 +0200

    Don't redirect to index after succesfully created a category

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -11,7 +11,7 @@ class Admin::CategoriesController < ApplicationController
   def create
     @category = Category.new(category_params)
     if @category.save
-      redirect_to admin_categories_path, notice: 'Category was successfully created.'
+      redirect_to edit_admin_category_path, notice: 'Category was successfully created.'
     else
       render :new
     end
