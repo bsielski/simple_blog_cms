0fffdbf3749cb70fca85c253f8205538a1dd8e0f
Bartlomiej (@VM) <b.sielski@yandex.com>
2017-06-23 21:04:48 +0200

    Reformat

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,13 +1,12 @@
 class Category < ApplicationRecord
 
-  
   has_many :categorizations
   has_many :articles, through: :categorizations
-
+  
   acts_as_list
   
   def self.default
     NonCategory.new
   end
-  
+
 end
