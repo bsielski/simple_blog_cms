00659a67b0ba3715dceeb18823dfeccb8a9b4e58
Bartlomiej (@VM) <b.sielski@yandex.com>
2017-06-24 23:47:26 +0200

    Fix pagination for all articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,12 +2,13 @@ class ArticlesController < ApplicationController
   def index
     if params[:category_id]
       @category = Category.find(params[:category_id].to_i)
-      @visible_articles = @category.articles.visible.paginate(:page => params[:page], :per_page => 10)
+      @visible_articles = @category.articles.visible
  
     else
       @visible_articles = Article.visible
     end
-    
+    @visible_articles = @visible_articles.paginate(:page => params[:page], :per_page => 10)
+
   end
   
 end
