c07f0de0ec9ac04a017aa91cd9e659c0f98cd7d7
Bartlomiej (@VM) <b.sielski@yandex.com>
2017-06-24 23:35:03 +0200

    Better pagination

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,7 @@ class ArticlesController < ApplicationController
   def index
     if params[:category_id]
       @category = Category.find(params[:category_id].to_i)
-      @visible_articles = @category.articles.visible.paginate(:page => params[:page], :per_page => 3)
+      @visible_articles = @category.articles.visible.paginate(:page => params[:page], :per_page => 10)
  
     else
       @visible_articles = Article.visible
