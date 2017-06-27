b05c40694be3423958b81f274151a49535b4f84c
Bartlomiej (@VM) <b.sielski@yandex.com>
2017-06-27 21:10:20 +0200

    Clear cache after saving stylesheets

--- app/models/stylesheet.rb
+++ app/models/stylesheet.rb
@@ -1,3 +1,14 @@
 class Stylesheet < ApplicationRecord
+  after_save :clear_cache
   has_one :site
+
+
+  private
+
+  def clear_cache
+    require 'rails/tasks'
+    Rake::Task['tmp:cache:clear'].execute
+  end
+  
+
 end
