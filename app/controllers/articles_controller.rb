f20bd7f8600d9a3e02d64333974b474a78517a55
Bartlomiej (@VM) <b.sielski@yandex.com>
2017-06-28 13:33:47 +0200

    Better performance for articles index

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,10 +5,9 @@ class ArticlesController < ApplicationController
   def index
     if params[:category_id]
       @category = Category.find(params[:category_id].to_i)
-      @visible_articles = @category.articles.visible
- 
+      @visible_articles = @category.articles.includes(:author, :categories).visible 
     else
-      @visible_articles = Article.visible
+      @visible_articles = Article.includes(:author, :categories).visible
     end
     @visible_articles = @visible_articles.paginate(:page => params[:page], :per_page => 10)
 
