25b270027268a5fed7c6382ae5ad6ca4ac547bc6
Bartlomiej (@VM) <b.sielski@yandex.com>
2017-06-30 18:55:47 +0200

    Add nesting in author in index

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,8 +4,11 @@ class ArticlesController < ApplicationController
 
   def index
     if params[:category_id]
-      @category = Category.find(params[:category_id].to_i)
-      @visible_articles = @category.articles.includes(:author, :categories).visible.reverse_order 
+      @category = Category.find(params[:category_id])
+      @visible_articles = @category.articles.includes(:author, :categories).visible.reverse_order
+    elsif params[:author_id]
+      @author = Author.find(params[:author_id])
+      @visible_articles = @author.articles.includes(:author, :categories).visible.reverse_order 
     else
       @visible_articles = Article.includes(:author, :categories).visible.reverse_order
     end
