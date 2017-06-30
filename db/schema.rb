4c272fb1dc66e204815398ff3f4c2386146c45b7
Bartlomiej (@VM) <b.sielski@yandex.com>
2017-06-30 16:27:13 +0200

    Add name to stylesheets migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170615165158) do
+ActiveRecord::Schema.define(version: 20170630141932) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -102,6 +102,7 @@ ActiveRecord::Schema.define(version: 20170615165158) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
   end
 
 end
