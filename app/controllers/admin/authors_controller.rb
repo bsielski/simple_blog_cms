af676150497ddef71ea0783f15237b32ac6112f5
Bartlomiej (@VM) <b.sielski@yandex.com>
2017-06-30 18:30:07 +0200

    Create actions

--- app/controllers/admin/authors_controller.rb
+++ app/controllers/admin/authors_controller.rb
@@ -1,16 +1,51 @@
 class Admin::AuthorsController < ApplicationController
+  
+  before_action :set_author, only: [:edit, :update, :delete, :destroy]
+  before_action :authenticate_admin!
+  
   def index
+    @authors = Author.includes(:author, :categories).visible.reverse_order
+    @authors = @authors.paginate(:page => params[:page], :per_page => 100)
   end
-
-  def show
-  end
-
+  
   def new
+    @author = Author.new
   end
 
-  def edit
+  def create
+    @author = Author.new(author_params)
+    if @author.save
+      redirect_to edit_admin_author_path(@author), notice: 'Author was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @author.update(author_params)
+      redirect_to edit_admin_author_path, notice: 'Author was successfully updated.'
+    else
+      render :edit
+    end
+
   end
 
   def delete
   end
+  
+  def destroy
+    @author.destroy
+    redirect_to admin_authors_path, notice: 'Author was successfully destroyed.'
+  end
+
+  private
+
+  def set_author
+    @author = Author.find(params[:id])
+  end
+
+  def author_params
+    params.require(:author).permit(:name, :admin_id)
+  end
+  
 end
