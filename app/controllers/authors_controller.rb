ece641ec5fe4df6c4bb935b6dca5a312fbb23546
Bartlomiej (@VM) <b.sielski@yandex.com>
2017-06-30 17:24:04 +0200

    Set author for ahow action

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,7 +1,17 @@
 class AuthorsController < ApplicationController
+
+  before_action :set_author, only: [:show]
+  
   def index
   end
 
   def show
   end
+
+  private
+  
+  def set_author
+    @author = Author.find(params[:id])
+  end
+    
 end
