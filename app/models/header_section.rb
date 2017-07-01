7528c810268e2df7d91957192d2113d4cd13b6ee
Bartlomiej (@VM) <b.sielski@yandex.com>
2017-07-01 18:04:23 +0200

    Add markdown methods

--- app/models/header_section.rb
+++ app/models/header_section.rb
@@ -1,2 +1,16 @@
 class HeaderSection < ApplicationRecord
+
+  before_validation :convert_content_to_html, on: [:create, :update]
+
+  def convert_content_to_markdown
+    self.content = Convert.to_markdown self.content
+  end
+  
+  private
+
+  def convert_content_to_html
+    self.content = Convert.to_html self.content
+  end
+
+
 end
