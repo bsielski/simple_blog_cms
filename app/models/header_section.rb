2492e8310978e3517b60d748ca3fa15de6ce60a6
Bartlomiej (@VM) <b.sielski@yandex.com>
2017-07-01 18:42:34 +0200

    Add acts_as_list

--- app/models/header_section.rb
+++ app/models/header_section.rb
@@ -2,6 +2,8 @@ class HeaderSection < ApplicationRecord
 
   before_validation :convert_content_to_html, on: [:create, :update]
 
+  acts_as_list
+  
   def convert_content_to_markdown
     self.content = Convert.to_markdown self.content
   end
