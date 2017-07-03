1106574307b414e8146e090ab58a006d9f70e39b
Bartlomiej (@VM) <b.sielski@yandex.com>
2017-07-03 17:06:02 +0200

    Display admin id when select or change author for post

--- app/models/author.rb
+++ app/models/author.rb
@@ -2,6 +2,10 @@ class Author < ApplicationRecord
   belongs_to :admin
   has_many :articles
 
+  def name_with_id
+    self.name + " (admin id: #{self.admin_id})"
+  end
+  
   def not_deleted?
     true
   end
