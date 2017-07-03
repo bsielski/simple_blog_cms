ae5f56e720f2f3ffe378b4a0527893376af630ba
Bartlomiej (@VM) <b.sielski@yandex.com>
2017-07-03 12:14:21 +0200

    Fill admin/admin controller

--- app/controllers/admin/admins_controller.rb
+++ app/controllers/admin/admins_controller.rb
@@ -1,7 +1,43 @@
 class Admin::AdminsController < ApplicationController
+  
+  before_action :set_admin, only: [:edit, :update, :delete, :destroy]
+  before_action :authenticate_admin!
+
+
   def index
+    @admins = Admin.includes(:authors, :roles).paginate(:page => params[:page], :per_page => 100)
+
   end
 
-  def show
+  def edit
   end
+
+  def update
+    if @admin.update(admin_params)
+      redirect_to edit_admin_admin_path, notice: 'Admin was successfully updated.'
+    else
+      render :edit
+    end
+
+  end
+
+  def delete
+
+  end
+  
+  def destroy
+    @admin.destroy
+    redirect_to admin_admins_path, notice: 'Admin was successfully destroyed.'
+  end
+
+  private
+
+  def set_admin
+    @admin = Admin.find(params[:id])
+  end
+    
+  def admin_params
+    params.require(:admin).permit(:email)
+  end
+
 end
