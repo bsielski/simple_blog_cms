73b9b00e4f1948cc9af6d0de5bf9bb00fee23094
Bartlomiej (@VM) <b.sielski@yandex.com>
2018-02-04 14:01:26 +0100

    Delete comments

--- run.sh
+++ run.sh
@@ -4,26 +4,11 @@ set -e
 if [ "$MIGRATIONS" != 0 ] && [ ! -z "$MIGRATIONS" ]
 then
   rails db:migrate
-#   echo migrations was 0
-#   MIGRATIONS=1
-#   echo migrations is now:
-#   echo $MIGRATIONS
-# else
-#   echo migrations is not 0
-#   echo is:
-#   echo $MIGRATIONS
 fi
 
 if [ "$SEED" != 0 ] && [ ! -z "$SEED" ]
 then
   rails db:seed
-  # SEED=1
-  # echo seed is:
-  # echo $SEED
-# else
-#   echo seed is not 0
-#   echo is:
-#   echo $SEED
 fi
 
 exec "$@"
