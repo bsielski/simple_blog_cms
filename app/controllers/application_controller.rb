3aa9cb0d517d49b4cdd5f31dc09816dba6e8a58f
Bartlomiej (@VM) <b.sielski@yandex.com>
2018-09-19 18:51:28 +0200

    Add automatically current_admin to oparations called by run

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,10 @@ class ApplicationController < ActionController::Base
 
   private
 
+  def _run_options(options)
+    options.merge( current_user: current_admin )
+  end
+
   def set_everything
     @current_style = Site.first.stylesheet.content
   end
