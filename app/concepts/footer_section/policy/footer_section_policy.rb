cb062c7839024855753a79512946b537a0f9e1d1
Bartlomiej (@VM) <b.sielski@yandex.com>
2018-09-20 20:29:19 +0200

    Add update policy

--- app/concepts/footer_section/policy/footer_section_policy.rb
+++ app/concepts/footer_section/policy/footer_section_policy.rb
@@ -9,6 +9,11 @@ class FooterSectionPolicy
     @admin.has_role? :super_admin or @admin.has_role? :can_create_footer_sections
   end
 
+  def update?
+    return false if @admin.nil?
+    @admin.has_role? :super_admin or @admin.has_role? :can_update_footer_sections
+  end
+
   def destroy?
     return false if @admin.nil?
     @admin.has_role? :super_admin or @admin.has_role? :can_destroy_footer_sections
