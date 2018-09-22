d635b7020ef7f44fe975d8f753433abf29ba18c9
Bartlomiej (@VM) <b.sielski@yandex.com>
2018-09-22 18:40:40 +0200

    Create Edit cell

---
+++ app/concepts/footer_section/cell/edit.rb
@@ -0,0 +1,20 @@
+module FooterSection::Cell
+  class Edit < Trailblazer::Cell
+
+    def errors
+      options[:form].errors
+    end
+
+    def page_headline_text
+      "Edit footer section: #{model.id}"
+    end
+
+    def policy
+      options[:policy]
+    end
+
+    def can_destroy
+      options[:policy].destroy?
+    end
+  end
+end
