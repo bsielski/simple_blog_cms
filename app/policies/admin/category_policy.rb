e6a11aaff9ebc04a9aec3993b35d68e48268ec2b
Bartlomiej (@VM) <b.sielski@yandex.com>
2018-09-23 19:42:57 +0200

    Delete old redundand policies

--- app/policies/admin/category_policy.rb
+++ app/policies/admin/category_policy.rb
@@ -1,9 +1,5 @@
 class Admin::CategoryPolicy < ApplicationPolicy
 
-  def create?
-    admin.has_role? :super_admin or admin.has_role? :can_create_categories
-  end
-
   def update?
     admin.has_role? :super_admin or admin.has_role? :can_edit_categories
   end
