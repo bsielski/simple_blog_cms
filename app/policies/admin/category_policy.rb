c87045c5b19a9a82839462b1e04f10613f8453e3
Bartłomiej Sielski <b.sielski.webdev@gmail.com>
2020-07-17 11:23:03 +0200

    Fix lack of policy bug

--- app/policies/admin/category_policy.rb
+++ app/policies/admin/category_policy.rb
@@ -1,5 +1,9 @@
 class Admin::CategoryPolicy < ApplicationPolicy
 
+  def create?
+    admin.has_role? :super_admin or admin.has_role? :can_create_categories
+  end
+
   def update?
     admin.has_role? :super_admin or admin.has_role? :can_edit_categories
   end
